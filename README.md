378124405bf84c898c34e6dc692038b1645cda6f
Hern@N <hnasar@gmail.com>
2024-12-14 19:20:20 -0300

    Update TurcoX

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Solana Token Creator
+ñ# Solana Token Creator
 
 Primary goal of this repo is to help to experiment with solana token creation process without diving too much into details.
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
+import Image from "next/image";
 
 export const Footer: FC = () => {
   return (
     <div className="">
-      <footer className="footer  mx-auto flex flex-row items-center bg-neutral p-2 text-center text-neutral-content">
-        <div className="pl-2">
+      <footer className="footer mx-auto flex flex-row items-center bg-neutral py-1 text-center text-neutral-content">
+        <div className="my-auto">
           <svg
             width="100%"
-            height="22"
+            height="70%"
             viewBox="0 0 101 88"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -36,36 +37,20 @@ export const Footer: FC = () => {
           </svg>
         </div>
         <div className="mx-auto max-w-md grid-flow-col gap-4 text-center sm:pl-12">
-          <div className="text-xl font-normal">Source code is available at</div>
-          <a
-            href="https://github.com/exitatmosphere/Solana-Token-Creator"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="fill-current"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="m12 .5c-6.63 0-12 5.28-12 11.792 0 5.211 3.438 9.63 8.205 11.188.6.111.82-.254.82-.567 0-.28-.01-1.022-.015-2.005-3.338.711-4.042-1.582-4.042-1.582-.546-1.361-1.335-1.725-1.335-1.725-1.087-.731.084-.716.084-.716 1.205.082 1.838 1.215 1.838 1.215 1.07 1.803 2.809 1.282 3.495.981.108-.763.417-1.282.76-1.577-2.665-.295-5.466-1.309-5.466-5.827 0-1.287.465-2.339 1.235-3.164-.135-.298-.54-1.497.105-3.121 0 0 1.005-.316 3.3 1.209.96-.262 1.98-.392 3-.398 1.02.006 2.04.136 3 .398 2.28-1.525 3.285-1.209 3.285-1.209.645 1.624.24 2.823.12 3.121.765.825 1.23 1.877 1.23 3.164 0 4.53-2.805 5.527-5.475 5.817.42.354.81 1.077.81 2.182 0 1.578-.015 2.846-.015 3.229 0 .309.21.678.825.56 4.801-1.548 8.236-5.97 8.236-11.173 0-6.512-5.373-11.792-12-11.792z"></path>
-            </svg>
-          </a>
+           
         </div>
         <div className="grid-flow-col gap-4 pr-2 text-center">
           <div>
             <p className="cursor-default text-base font-light text-white ">
-              Powered by
+              Contact us
             </p>
-            <a
-              rel="noreferrer"
-              href="https://solana.com/"
-              target="_blank"
-              className="hover:text-primary-dark text-base font-bold text-white transition-all duration-200"
-            >
-              Solana
-            </a>
+            <Image
+            src="/turcox.png"
+            alt="TurcoX"
+            width={88}
+            height={88}
+            priority
+          />
           </div>
         </div>
       </footer>
